31eac238618563524b74a65f580eafc553f824e8
yepes0 <71985093+yepes0@users.noreply.github.com>
2025-08-30 02:41:55 +0000

    listoooo papiiii

---
+++ app/controllers/division_controller.rb
@@ -0,0 +1,14 @@
+class DivisionController < ApplicationController
+  def show_division_form
+    render({ :template => "division_templates/division_form" })
+  end
+
+  def divide_these
+    @first_number = params.fetch("first_number").to_f
+    @second_number = params.fetch("second_number").to_f
+
+    @result =  @first_number / @second_number
+
+    render({ :template => "division_templates/divide_results" })
+  end
+end
